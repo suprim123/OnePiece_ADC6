206dfe5b673fa84c592c65835b04d0e421c6b3f8
NaemiShrestha01 <59082549+NaemiShrestha01@users.noreply.github.com>
2020-01-24 19:05:10 +0545

    testing done

--- HostelFinding/Hostel/models.py
+++ HostelFinding/Hostel/models.py
@@ -12,3 +12,23 @@ class Hostel(models.Model):
     def __str__(self):
      return f"{self.Name} has record {self.Location}"
 
+
+     def uploaddata_name_blank(self):
+        if self.Name == "":
+            return True
+        else:
+            return False
+
+   
+    def uploadata_fields_blank(self):
+        return(self.Name != True)
+
+    def TestWork(self):
+        return (work_HO((self.Location) >= 3) and work_HO((self.Location) <= 50))
+       
+    def Price_fields_blank(self):
+        return (self.Price!= False)
+ 
+    def workdes_field_blank(self):
+        return ((self.Description != True ) and (self.Description) <= 50)
+
