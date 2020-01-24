9704120df094ea254ee5eedb1f7b195dcf0c2a69
Dikshya-max <58898966+Dikshya-max@users.noreply.github.com>
2020-01-24 19:04:06 +0545

    Front_end and backend Done
    template(Signup,Login,logout,signin)
    back-end(signup,login,logout,signin)

--- HostelFinding/Hostel/models.py
+++ HostelFinding/Hostel/models.py
@@ -2,16 +2,32 @@ from django.db import models
 
 # Create your models here.
 class Hostel(models.Model):
-    Name = models.CharField(max_length=25, null= False)
-    Location = models.CharField(max_length=50)
+    Name = models.CharField(max_length=25)
     Price = models.CharField(max_length=7)
-    Picture = models.FileField()
     Description = models.TextField()
-
+    addresses = models.ManyToManyField('Location')
 
 
     def __str__(self):
      return f"{self.Name} has record {self.Location}"
+
+     
+
+class Location(models.Model):
+    Name=models.CharField(max_length=50)
+    Location=models.CharField(max_length=50)
+      
+
+class HostelDetails(models.Model):
+	Description = models.CharField(max_length=20)
+	Hostel = models.OneToOneField(Hostel, on_delete = models.CASCADE)
+	Price = models.IntegerField(default=0)
+	
+
+class user(models.Model):
+    Name = models.CharField(max_length = 50)
+    Location = models.CharField(max_length=30)
+    Hostels = models.ForeignKey(Hostel, on_delete=models.CASCADE)
 
 
 
@@ -35,16 +51,3 @@ class Hostel(models.Model):
         return ((self.Description != True ) and (self.Description) <= 50)
 
 
-# Create your models here.
-
-
-# class Hostel(models.Model):
-# 	name = models.CharField(max_length = 50)
-# 	address = models.CharField(max_length=30)
-# 	contactNum = models.IntegerField(max_length=10)	
-
-# class HostelDetails(models.Model):
-# 	facilities = models.CharField(max_length=20)
-# 	hostel = models.ForeignKey(Hostel, on_delete = models.CASCADE)
-# 	price = models.IntegerField(default=0)
-# 	num_of_seats = models.IntegerField(default=0)
--- HostelFinding/Hostel/tests.py
+++ HostelFinding/Hostel/tests.py
@@ -1,42 +1,34 @@
-# from django.test import TestCase, Client
-# from .models import Hostel
-# # from django.url import reverse, resolve
-
-# class ModelsTestcase(TestCase):
-#     def test_uploaddata_location_blank(self):
-#         ho =Hostel.objects.create(Name="dikshya")
-#         self.assertTrue(ho.uploaddata_location_blank())
 
 
-from django.test import TestCase, Client
-from .models import Hostel
-from django.urls import reverse, resolve
+    from django.test import TestCase, Client
+    from .models import Hostel
+    from django.urls import reverse, resolve
 
 
 
-# Create your tests here.
-class ModelTestCase(TestCase):
-    def setUp(self):
-        hostel = Hostel.objects.create(Name="ram")
-        location = Hostel.objects.create(Location="japan")
-        price = Hostel.objects.create(Price= '900')
+    # Create your tests here.
+    class ModelTestCase(TestCase):
+        def setUp(self):
+            hostel = Hostel.objects.create(Name="ram")
+            location = Hostel.objects.create(Location="japan")
+            price = Hostel.objects.create(Price= '900')
 
 
-    def test_name(self):
-        hostel = Hostel.objects.get(Name = "ram")
-        self.assertEqual(hostel.Name, "naemi")
+        def test_name(self):
+            hostel = Hostel.objects.get(Name = "ram")
+            self.assertEqual(hostel.Name, "naemi")
 
+            
+        def test_location(self):
+            location = Hostel.objects.get(Location = "japan")
+            self.assertEqual(location.Location, "dsajk")
+
+
+        def test_price(self):
+            price = Hostel.objects.get(Price = '900')
+            self.assertEqual(price.Price, '808')
+
+        def test_description(self):
+        description = Hostel.objects.get(Description = 'lalallalalallalallalala')
+            self.assertEqual(description.Description, 'lalallalalallalallalala')
         
-    def test_location(self):
-        location = Hostel.objects.get(Location = "japan")
-        self.assertEqual(location.Location, "dsajk")
-
-
-    def test_price(self):
-        price = Hostel.objects.get(Price = '900')
-        self.assertEqual(price.Price, '808')
-
-    def test_description(self):
-       description = Hostel.objects.get(Description = 'lalallalalallalallalala')
-        self.assertEqual(description.Description, 'lalallalalallalallalala')
-    
